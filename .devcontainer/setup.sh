c0accc24b1136363c1bab3b67aa13df52a10b39a
Ayman Wadud <aymanwadud2@gmail.com>
2024-12-20 14:59:09 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,11 +2,11 @@
 
 echo "Updating SQLite to version 3.35.0+"
 
-# Install prerequisites
+# Install build tools and SQLite dependencies
 sudo apt update
-sudo apt install -y build-essential libsqlite3-dev
+sudo apt install -y build-essential libsqlite3-dev wget
 
-# Download and build SQLite 3.35.0+
+# Download and build the latest SQLite
 wget https://www.sqlite.org/2024/sqlite-autoconf-3420000.tar.gz
 tar -xzf sqlite-autoconf-3420000.tar.gz
 cd sqlite-autoconf-3420000
@@ -14,5 +14,10 @@ cd sqlite-autoconf-3420000
 make
 sudo make install
 
-# Verify the version
+# Verify SQLite version
 sqlite3 --version
+
+# Rebuild Python to link with the updated SQLite library
+cd ..
+pyenv install 3.12.0 --force
+pyenv global 3.12.0
